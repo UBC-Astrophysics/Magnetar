4c3ae1ec169b23f8e1a60b25ce76e43cf664d293
jsheyl <heyl@phas.ubc.ca>
2022-11-11 13:39:35 -0800

    Update condensed.py

--- Magnetar/condensed.py
+++ Magnetar/condensed.py
@@ -1,15 +1,15 @@
     
-from numpy import sin, cos, arccos, radians, exp, expm1, log, sqrt, minimum, maximum, where, pi, logspace, array
+from numpy import sin, cos, arccos, radians, exp, expm1, log, sqrt, minimum, maximum, where, pi, logspace, array, zeros
 from Magnetar.utils import atmosphere
 from scipy.integrate import simps
 from Magnetar.simple_atmospheres import bbfunk
 
 import ctypes
-_sum = ctypes.CDLL('emissivity.so')
+_sum = ctypes.CDLL('Magnetar/emissivity.so')
 _sum.emissivity.argtypes = (ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.c_int,
                             ctypes.c_double,ctypes.c_double, ctypes.c_double,ctypes.c_double, ctypes.c_double,ctypes.c_int,
                             ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-_sum.emissivity.restype = ctypes.c_void
+_sum.emissivity.restype = None
 
 class condensed_surface(atmosphere):
     def __init__(self,effective_temperature,mag_strength,mag_inclination,density,fixed_ions=True):
@@ -47,19 +47,20 @@ class condensed_surface(atmosphere):
         thetak=radians(dataarray[-3])
         phik=radians(dataarray[-2])
         ene=dataarray[-1]
-        emisX=0*thetak
-        emisO=0*thetak
         lx=len(thetak)
         array_type=ctypes.c_double * lx
-        if fixed_ions
+        eXout=array_type(*zeros(lx))
+        eOout=array_type(*zeros(lx))
+        if fixed_ions:
             fion=1
         else:
             fion=0
             
         _sum.emissivity(array_type(*thetak),array_type(*phik),array_type(*ene),ctypes.c_int(lx),
-                                  ctypes.c_double(mag_strength),ctypes.c_double(mag_inclination),ctypes.c_double(dens),
-                                  ctypes.c_double(Z),ctypes.c_double(A),ctypes.c_int(fion),array_type(*emisX),array_type(*emisO))
-        return emisX,emisO
+                                  ctypes.c_double(mag_strength),ctypes.c_double(radians(mag_inclination)),ctypes.c_double(dens),
+                                  ctypes.c_double(Z),ctypes.c_double(A),ctypes.c_int(fion),eXout,eOout)
+        return array(eXout),array(eOout)
+
 
 
     def emissivity_xo(self,dataarray):
